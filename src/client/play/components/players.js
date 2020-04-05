ea577266b6ddc6c5b448b717985a0d0c4989fd4e
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2020-04-04 22:32:37 -0700

    Allows to show/hide player panel
    With lots of players that can get in the way.

--- src/client/play/components/players.js
+++ src/client/play/components/players.js
@@ -4,15 +4,35 @@ Vue.component('players', {
     <div
       v-if="playerOrder"
       class="players"> 
-      <h2>Players</h2>
-      <ol>
-        <li
-          class="player"
-          v-for="(player, index) in playerOrder"
-          v-bind:key="player.id">
-          {{currentIndex === index ? '▶' : ''}} {{ player.name }} - {{playerSizes[index]}} dominoes
-        </li>
-      </ol>
+      <div
+        class="player-content"
+        v-bind:class="{ hidden: playersHidden }">
+        <h2>Players</h2>
+        <ol>
+          <li
+            class="player"
+            v-for="(player, index) in playerOrder"
+            v-bind:key="player.id">
+            {{currentIndex === index ? '▶' : ''}} {{ player.name }} - {{playerSizes[index]}} dominoes
+          </li>
+        </ol>
+      </div>
+      <button
+        v-on:click="togglePlayers()"
+        class="show-toggle-button">{{ toggleText }}</button>
     </div>
-  `
+  `,
+  data: function () {
+    return { playersHidden: false };
+  },
+  computed: {
+    toggleText() {
+      return this.playersHidden ? 'show players' : 'x';
+    }
+  },
+  methods: {
+    togglePlayers() {
+      this.playersHidden = !this.playersHidden;
+    }
+  }
 });
