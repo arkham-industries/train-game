35ba5dbbc8841975d8e11f01cd595b14e3cbc380
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2020-04-06 21:17:51 -0700

    Redirects user from create game button
    We don't need the join link anymore (yay).

--- src/client/entry/main.js
+++ src/client/entry/main.js
@@ -1,16 +1,10 @@
-function showLink(joinCode) {
-  const url = `/join/${joinCode}`;
-  document.getElementById('join-link').setAttribute('href', url);
-  document.getElementById('join-link').text = 'Join Game';
-}
-
 function startGame() {
   fetch('game', { method: 'POST'})
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    showLink(json.joinCode);
+    window.location.href = `/join/${json.joinCode}`;
   });
 }
 
