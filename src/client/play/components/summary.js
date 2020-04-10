1829211a479f1c8447d38871c6fa4e4acca4814c
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2020-04-09 17:32:43 -0700

    Renders all rounds plus total

--- src/client/play/components/summary.js
+++ src/client/play/components/summary.js
@@ -5,16 +5,30 @@ Vue.component('game-summary', {
       class="centered-container">
       <h1>Round Ended!</h1>
       <table class="game-summary">
-        <tr v-for="player in playerOrder">
+        <tr v-for="(player, index) in playerOrder">
           <td>{{ player.name }}</td>
           <td 
-            v-for="score in player.scores"
+            v-for="round in rounds"
             class="score-cell">
-            {{score.value}}
+            {{player.scores[round]?.value || '-'}}
           </td>
+          <td 
+          class="score-cell total">
+          {{totals[index]}}
+        </td>
         </tr>
       </table>
       <button class="big-button" v-on:click="$emit('start-new-game')">Start Next Round!</button>
     </div>
-  `
+  `,
+  data: function() {
+    return { rounds: [...Array(13).keys()] }
+  },
+  computed: {
+    totals() {
+      return this.playerOrder.map((player) => {
+        return player.scores.reduce((acc, score) => acc += score.value, 0);
+      }, 0);
+    }
+  }
 })
