deacc809b86af97bfb9dbd82918ab571fcb4fecd
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2020-04-20 19:53:45 -0700

    Adds bugsnag to app
    So we can get some clues into debugging old iPad issues.

--- src/client/play/main.js
+++ src/client/play/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import 'whatwg-fetch';  // polyfill for window.fetch
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
+
+if (process.env.BUGSNAG_API_KEY && process.env.NODE_ENV === 'production') {
+  Bugsnag.start({
+    apiKey: process.env.BUGSNAG_API_KEY,
+    plugins: [new BugsnagPluginVue(Vue)]
+  })
+}
 
 new Vue({ render: createElement => createElement(App) }).$mount('#app');
